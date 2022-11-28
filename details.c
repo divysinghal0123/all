1221560b0b5715c09d7670e25595c618a06e65c0
Divy Singhal <76985594+divysinghal0123@users.noreply.github.com>
2022-11-28 09:56:51 +0530

    Create details.c

---
+++ details.c
@@ -0,0 +1,10 @@
+1.Study of necessary header files with respect to socket programming.
+2. Study of Basic Functions of Socket Programming
+3. Simple TCP/IP Client Server Communication
+4. UDP Echo Client Server Communication
+5. Concurrent TCP/IP Day-Time Server
+6. Half Duplex Chat Using TCP/IP
+7. Full Duplex Chat Using TCP/IP
+8. Implementation of File Transfer Protocol
+9. Remote Command Execution Using UDP
+10. ARP Implementation Using UDP
